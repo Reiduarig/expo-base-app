a3001c649b4f887aaa530c96427d0eaba0c220e4
Reiduarig <giraudier82@gmail.com>
2025-12-10 13:53:38 +0100

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,9 +2,9 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -20,14 +20,21 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color, focused }) => <Ionicons size={28} name={focused ? 'home' : 'home-outline'} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'About',
+          tabBarIcon: ({ color, focused }) => <Ionicons size={28} name={focused ? 'information-circle' : 'information-circle-outline'} color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color, focused }) => <Ionicons size={28} name={focused ? 'compass' : 'compass-outline'} color={color} />,
         }}
       />
     </Tabs>
--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -2,13 +2,14 @@ import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { StyleSheet } from 'react-native';
 
-export default function TabTwoScreen() {
-  return (
-      <ThemedView style={styles.container}>
-          <ThemedText style={styles.text}>Explore screen</ThemedText>
-      </ThemedView>
+export default function AboutScreen() {
+    return (
+        <ThemedView style={styles.container}>
+            <ThemedText style={styles.text}>About screen</ThemedText>
+        </ThemedView>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
