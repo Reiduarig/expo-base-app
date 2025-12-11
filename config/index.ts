c58e17d89309747f29cbd5e4f1c8ec9c0e68e909
Reiduarig <giraudier82@gmail.com>
2025-12-11 12:03:44 +0100

    archivo indice para las importaciones de configuracion

---
+++ config/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Archivo de índice para facilitar las importaciones de configuración
+ */
+
+export { APP_CONFIG } from './constants';
+export type { ErrorCode } from './constants';
+export { ENV, validateEnv } from './env';
+
