2f6a2e41d64d1234712a21403811ae3519d95a13
Reiduarig <giraudier82@gmail.com>
2025-12-11 12:09:04 +0100

    archivo indice para las importaciones de servicios

---
+++ services/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Archivo de índice para facilitar las importaciones de servicios
+ */
+
+export { apiClient } from './api/client';
+export { API_ENDPOINTS } from './api/endpoints';
+export { authService } from './auth.service';
+export { userService } from './user.service';
+
